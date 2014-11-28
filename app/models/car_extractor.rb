8ec086c2e0667a4ea3202ecabadf1b7648d9f5cc
Mwaki Magotswi <magotswi@gmail.com>
2014-11-28 06:34:26 -0500

    setting up specs

--- app/models/car_extractor.rb
+++ app/models/car_extractor.rb
@@ -4,11 +4,18 @@ class CarExtractor
   attr_reader :cars
   
   def initialize
-    @page = fetch
-    @cars = @page.css('tr').collect{|x| [x.text]}
+    @cars = get_car_array(fetch)
   end
   
-  protected
+  def get_car_array(page)
+    page.css('tr').collect{|x| x.css('td')}.reject{|x| x.empty?}.collect{|x| [x.first.content, x[1].content, x.last.content]}
+  end
+  
+  def remove_empty_arrays(collection)
+    collection.reject{|array| array.empty?}
+  end
+  
+  private
   
   def fetch
     Nokogiri::HTML(open("http://www.iihs.org/iihs/ratings/vehicles-for-teens"))
--- test/models/car_extractor_test.rb
+++ test/models/car_extractor_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CarExtractorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should remove empty arrays" do
+    array1 = [[], [1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    array2 = [[1, 2, 3], [], [4, 5, 6], [7, 8, 9]]
+    array3 = [[1, 2, 3], [4, 5, 6], [], [7, 8, 9]]
+    array4 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], []]
+    array5 = [[], [1, 2, 3], [], [4, 5, 6], [], [7, 8, 9], []]
+    array6 = [[], [], [], [1, 2, 3], [], [], [], [4, 5, 6], [], [], [], [7, 8, 9], [], [], []]
+    array7 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    [array1, array2, array3, array4, array5, array6, array7].each do |test_array|
+      assert_equal CarExtractor.new.remove_empty_arrays(test_array), [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    end
+  end
+  
+  test "should handle empty arrays" do
+    array1 = [[]]
+    array2 = [[], [], []]
+    array3 = []
+    
+    [].each do |test_array|
+      assert_equal CarExtractor.new.remove_empty_arrays(test_array), []
+    end
+  end
 end

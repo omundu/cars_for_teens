03e753cc78af67353ccacdddccd3e920da567f1d
Mwaki Magotswi <magotswi@gmail.com>
2015-04-05 17:35:11 -0400

    moving data acquisition logic around, cleaner calls

--- app/controllers/car_controller.rb
+++ app/controllers/car_controller.rb
@@ -1,9 +1,8 @@
 class CarController < ApplicationController
   def model
     @title = "#{params[:manufacturer]} #{params[:model]}"
-    @garage = CarExtractor.new
     
-    @years = @garage.cars.select{|car| car.model == params[:model]}.map(&:years).flatten
+    @years = cars.select{|car| car.model == params[:model]}.map(&:years).flatten
   end
   
   def year
--- app/controllers/manufacturer_controller.rb
+++ app/controllers/manufacturer_controller.rb
@@ -1,15 +1,13 @@
 class ManufacturerController < ApplicationController
   def index
     @title = "Manufacturers"
-    @garage = CarExtractor.new
     
-    @manufacturers = @garage.cars.map(&:manufacturer).uniq
+    @manufacturers = cars.map(&:manufacturer).uniq
   end
   
   def show
     @title = params[:manufacturer]
-    @garage = CarExtractor.new
     
-    @cars = @garage.cars.select{|car| car.manufacturer == params[:manufacturer]}
+    @cars = cars.select{|car| car.manufacturer == params[:manufacturer]}
   end
 end

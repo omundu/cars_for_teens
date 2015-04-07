008089e0940e43b6e30916a441813a8b482cab22
Mwaki Magotswi <magotswi@gmail.com>
2015-04-07 04:25:50 -0400

    Fetching data from wikipedia to augment site
    - Case sensitive so needed logic to handle that
    - Handles pages with redirects successfully
    - BMW and Mercedes were at it again…3 series vs C-Class naming screwed
    a number of things up
    
    - A bit unpolished and non-dry…to handle later
    - Styling needs to be addressed, the simple layout is unhappy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def get_wikipedia_summary(urls)
+    abstract_url = "http://dbpedia.org/ontology/abstract"
+    redirect_url = "http://dbpedia.org/ontology/wikiPageRedirects"
+  
+    article_data = fetch_article(urls)
+    
+    unless article_data[redirect_url].nil?
+      redirects = article_data[redirect_url]
+      urls[:resource_url] = redirects.first["value"]
+      urls[:data_url] = urls[:resource_url].gsub("resource", "data") + ".json"
+      
+      article_data = fetch_article(urls)
+    end
+    
+    summary_data = article_data[abstract_url]
+    
+    # Extract the summary paragraph in english
+    summary_data.select{|data| data["lang"] == "en"}.first["value"]
+  end
+  
+  def fetch_article(urls)
+    all_data = JSON.parse(open(urls[:data_url]).read)
+    all_data[urls[:resource_url]]
+  end
 end
--- app/helpers/manufacturer_helper.rb
+++ app/helpers/manufacturer_helper.rb
@@ -1,2 +1,41 @@
 module ManufacturerHelper
+  
+  WIKIPEDIA_NAMES = {
+    "Saab" => "Saab_Automobile",
+    "Lincoln" => "Lincoln_Motor_Company",
+    "Buick" => "Buick",
+    "Ford" => "Ford_Motor_Company",
+    "Volvo" => "Volvo_Cars",  
+    "Toyota" => "Toyota",
+    "Mercedes-Benz" => "Mercedes-Benz",
+    "Honda" => "Honda",
+    "Audi" => "Audi",
+    "Subaru" => "Subaru",
+    "Kia" => "Kia_Motors",
+    "Hyundai" => "Hyundai_Motor_Company", 
+    "Dodge" => "Dodge",
+    "Volkswagen" => "Volkswagen_Group",
+    "Chevrolet" => "Chevrolet",
+    "Chrysler" => "Chrysler",
+    "Mercury" => "Mercury_(automobile)",
+    "Mitsubishi" => "Mitsubishi_Motors",
+    "GMC" => "GMC_(automobile)",
+    "Infiniti" => "Infiniti" ,
+    "Acura" => "Acura",
+    "BMW" => "BMW",
+    "Mazda" => "Mazda",
+    "Saturn" => "Saturn_Corporation",
+    "Suzuki" => "Suzuki",
+    "Nissan" => "Nissan_Motor_Company"
+  }
+  
+  def get_manufacturer_summary
+    manufacturer = params[:manufacturer]
+    
+    data_url = "http://dbpedia.org/data/#{WIKIPEDIA_NAMES[manufacturer]}.json"
+    resource_url = "http://dbpedia.org/resource/#{WIKIPEDIA_NAMES[manufacturer]}"
+    
+    get_wikipedia_summary({data_url: data_url, resource_url: resource_url})
+  end
+
 end

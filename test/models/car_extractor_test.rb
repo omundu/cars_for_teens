eadcac4e288201f291c41850924c2f92198b189f
Mwaki Magotswi <magotswi@gmail.com>
2015-04-24 16:59:53 -0400

    going for messy but green specs

--- test/models/car_extractor_test.rb
+++ test/models/car_extractor_test.rb
@@ -2,16 +2,30 @@ require 'test_helper'
 
 class CarExtractorTest < ActiveSupport::TestCase
   test "get car array" do
-    skip "Object equality needs to be addressed"
-    
-    html_doc = Nokogiri::HTML("<html><body><table><tr><td>one</td><td>two</td></tr><tr><td>three</td><td>four</td></tr><tr><td>five</td><td>six</td></tr></table></body></html>")
+    html_doc = Nokogiri::HTML(
+      "<html>
+        <body>
+          <table>
+            <tr>
+              <td>kia rio</td><td>2008-10</td>
+            </tr>
+            <tr>
+              <td>vw ion</td><td>2011</td>
+            </tr>
+            <tr>
+              <td>gmc tc</td><td>2012 and later</td>
+            </tr>
+          </table>
+        </body>
+      </html>"
+    )
     expected_result = [
-      Car.new(manufacturer: "one", year: "two", suggested_price: 0, model: ""),
-      Car.new(manufacturer: "three", year: "four", suggested_price: 0, model: ""),
-      Car.new(manufacturer: "five", year: "six", suggested_price: 0, model: "")
+      Car.new({manufacturer: "kia", years: [2008, 2009, 2010], suggested_price: 0, model: "rio"}),
+      Car.new({manufacturer: "vw", years: [2011], suggested_price: 0, model: "ion"}),
+      Car.new({manufacturer: "gmc", years: [2012, 2013, 2014], suggested_price: 0, model: "tc"})
     ]
     
-    assert_equal expected_result, CarExtractor.new.get_car_array(html_doc)
+    assert_equal expected_result.map(&:to_s), CarExtractor.new.get_car_array(html_doc).map(&:to_s)
   end
   
   test "remove empty arrays" do
@@ -39,21 +53,100 @@ class CarExtractorTest < ActiveSupport::TestCase
   end
   
   test "select by elements" do
-    html_doc = Nokogiri::HTML("<html><body><ul><li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li></ul></body></html>")
+    html_doc = Nokogiri::HTML(
+      "<html>
+        <body>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+          </ul>
+        </body>
+      </html>"
+    )
     extracted = CarExtractor.new.select_by_element(html_doc, "li").map(&:content)
     
     assert_equal ["1", "2", "3", "4", "5", "6"], extracted
   end
   
-  test "select data cells" do
-    skip("need to properly define the call")
+  test "select data cells" do    
+    html_doc = Nokogiri::HTML(
+      "<html>
+        <body>
+          <table>
+            <tr>
+              <td>one</td><td>two</td>
+            </tr>
+            <tr>
+              <td>three</td><td>four</td>
+            </tr>
+            <tr>
+              <td>five</td><td>six</td>
+            </tr>
+          </table>
+        </body>
+      </html>"
+    )
+    expected_result = [
+      Nokogiri::HTML("<tr><td>one</td><td>two</td></tr>").at_css('tr').children,
+      Nokogiri::HTML("<tr><td>three</td><td>four</td></tr>").at_css('tr').children,
+      Nokogiri::HTML("<tr><td>five</td><td>six</td></tr>").at_css('tr').children
+    ]
+    received_result = CarExtractor.new.select_data_cells(html_doc)
     
-    html_doc = Nokogiri::HTML("<html><body><table><tr><td>one</td><td>two</td></tr><tr><td>three</td><td>four</td></tr><tr><td>five</td><td>six</td></tr></table></body></html>")
-    selected = CarExtractor.new.select_data_cells(html_doc)
+    assert_equal expected_result.map(&:text), received_result.map(&:text)
   end
   
   test "extract car information" do
-    skip("need to understand the data output")
+    car =  Nokogiri::HTML("<tr><td>vw ion</td><td>2011</td></tr>").at_css('tr').css('td')
+    expected_car = Car.new({manufacturer: "vw", years: [2011], suggested_price: 0, model: "ion"})
+    built_car = CarExtractor.new.extract_car_information([car]).first
+    
+    assert_equal expected_car.to_s, built_car.to_s
+  end
+  
+  test "construct car information" do
+    row0 = Nokogiri::HTML("<tr><td>ford puma</td><td>2006-08</td></tr>").at_css('tr').css('td')
+    row1 = Nokogiri::HTML("<tr><td>fiat pogo</td><td>2009</td></tr>").at_css('tr').css('td')
+    row2 = Nokogiri::HTML("<tr><td>kia rogue</td><td>2010 and later</td></tr>").at_css('tr').css('td')
+    row3 = Nokogiri::HTML("<tr><td>bmw X-5 M</td><td>2005-08; built after October 2004</td></tr>").at_css('tr').css('td')
+    
+    car0 = {
+      :manufacturer => "ford",
+      :model => "puma",
+      :years => [2006, 2007, 2008],
+      :suggested_price => 2006,
+      :extra_information => nil
+    }
+    car1 = {
+      :manufacturer => "fiat",
+      :model => "pogo",
+      :years => [2009],
+      :suggested_price => 2009,
+      :extra_information => nil
+    }
+    car2 = {
+      :manufacturer => "kia",
+      :model => "rogue",
+      :years => [2010, 2011, 2012, 2013, 2014],
+      :suggested_price => 2010,
+      :extra_information => nil
+    }
+    car3 = {
+      :manufacturer => "bmw",
+      :model => "X-5 M",
+      :years => [2005, 2006, 2007, 2008],
+      :suggested_price => 2005,
+      :extra_information => " built after October 2004"
+    }
+    
+    assert_equal car0, CarExtractor.new.construct_car_information(row0)
+    assert_equal car1, CarExtractor.new.construct_car_information(row1)
+    assert_equal car2, CarExtractor.new.construct_car_information(row2)
+    assert_equal car3, CarExtractor.new.construct_car_information(row3)
   end
   
   test "extract years" do

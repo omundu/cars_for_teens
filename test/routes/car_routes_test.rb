6ea2c24363bf7388fc3daef2bfdf4c58222679c3
Mwaki Magotswi <magotswi@gmail.com>
2015-04-25 07:58:22 -0400

    fixing routing specs in response to using test:all

--- test/routes/car_routes_test.rb
+++ test/routes/car_routes_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class CarRoutesTest < ActionController::TestCase
   test "should resolve the model url" do
-    assert_routing 'manufacturer/fiat/ka', {controller: "car", action: "model"}
+    assert_routing 'manufacturer/fiat/ka', {controller: "car", action: "model", manufacturer: "fiat", model: "ka"}
   end
  
   test "should resolve the model year url" do
-    assert_routing 'manufacturer/kia/rio/1010', {controller: "car", action: "year"}
+    assert_routing 'manufacturer/kia/rio/1010', {controller: "car", action: "year", manufacturer: "kia", model: "rio", year: "1010"}
   end
 end
--- test/routes/manufacturer_routes_test.rb
+++ test/routes/manufacturer_routes_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class ManufacturerRoutesTest < ActionController::TestCase
   test "should resolve the landing" do
-    assert_routing '/manufacturers', {controller: "manufacturers", action: "index"}
+    assert_routing '/manufacturers', {controller: "manufacturer", action: "index"}
   end
  
   test "should resolve all other actions to the landing page" do
-    ['/hi', '/test', '/foo', '/path', '/root', '/bar', '/banana', '/a'].each do |name|
-      assert_routing path, {controller: "manufactuer", action: "show", manufacturer: name}
+    ['hi', 'test', 'foo', 'a', 'mer-ben', 'FCA', 'bananarama', 'a_b-b_a'].each do |name|
+      assert_routing "manufacturer/#{name}", {controller: "manufacturer", action: "show", manufacturer: name}
     end
   end
 end

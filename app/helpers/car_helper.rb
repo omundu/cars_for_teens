1c706fdd8e8b01ea6efd818a13778189f576b045
Mwaki Magotswi <magotswi@gmail.com>
2015-04-27 14:23:10 -0400

    Moving wikipedia-related responsibilities to one place

--- app/helpers/car_helper.rb
+++ app/helpers/car_helper.rb
@@ -1,23 +1,2 @@
 module CarHelper
-  
-  def get_model_summary
-    manufacturer = params[:manufacturer]
-    model = stanitze_model_names
-    
-    data_url = "http://dbpedia.org/data/#{manufacturer}_#{model}.json"
-    resource_url = "http://dbpedia.org/resource/#{manufacturer}_#{model}"
-    
-    puts data_url
-    puts resource_url
-    
-    get_wikipedia_summary({data_url: data_url, resource_url: resource_url})
-  end
-  
-  def stanitze_model_names
-    # we need to remove "sedan" and join with underscore
-    model = params[:model].split(" ").reject{|word| word == "sedan" || word == "Tribeca/B9"}.join("_").gsub("3-series", "3_Series")
-    puts model
-    model
-  end
-    
 end

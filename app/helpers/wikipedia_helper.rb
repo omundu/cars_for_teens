5119d9f26ec77c83cb2d2635ec03090859a4435b
Mwaki Magotswi <magotswi@gmail.com>
2015-04-27 14:23:10 -0400

    Moving wikipedia-related responsibilities to one place

--- app/helpers/wikipedia_helper.rb
+++
@@ -1,2 +0,0 @@
-module WikipediaHelper
-end
--- test/helpers/wikipedia_helper_test.rb
+++ test/helpers/wikipedia_helper_test.rb
@@ -1,4 +1,44 @@
 require 'test_helper'
 
 class WikipediaHelperTest < ActionView::TestCase
+  
+  test "get manufacturer summary" do
+    skip "Awaiting refactor"
+    assert_equal get_manufacturer_summary, ""
+  end
+  
+  test "get wikipedia summary" do
+    skip "Awaiting refactor"
+    urls = ""
+    assert_equal get_wikipedia_summary(urls), ""
+  end
+  
+  test "fetch article" do
+    skip "Awaiting refactor"
+    urls = ""
+    assert_equal fetch_article(urls), ""
+  end
+  
+  test "fetch_article_data(urls)" do
+    skip "Awaiting refactor"
+    urls = ""
+    assert_equal fetch_article_data(urls), ""
+  end
+  
+  test "fetch article summary" do
+    skip "Awaiting refactor"
+    article_data = ""
+    assert_equal fetch_article_summary(article_data), ""
+  end
+  
+  test "get model summary" do
+    skip "Awaiting refactor"
+    assert_equal get_model_summary, ""
+  end
+  
+  test "stanitze model names" do
+    skip "Awaiting refactor"
+    assert_equal stanitze_model_names, ""
+  end
+  
 end

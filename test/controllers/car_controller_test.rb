6333de0ebae42f2e65aca9ea8b4f164ffed623cc
Mwaki Magotswi <magotswi@gmail.com>
2015-05-29 16:56:40 -0400

    A couple of sanity tests

--- test/controllers/car_controller_test.rb
+++ test/controllers/car_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CarControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get manufacturer" do
+    get :model, manufacturer: "Mitsubishi", model: "Pajero"
+    
+    assert_response :success
+  end
+  
+  test "should get model" do
+    get :year, manufacturer: "Mitsubishi", model: "Pajero", year: "1988"
+    
+    assert_response :success
+  end
 end
--- test/controllers/wikipedia_controller_test.rb
+++ test/controllers/wikipedia_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class WikipediaControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get manufacturer" do
+    get :manufacturer, manufacturer: "Mitsubishi"
+    
+    assert_response :success
+    assert_template :partial => false
+  end
+  
+  test "should get model" do
+    skip("sanitizing name gets nil [] error, need to stub data")
+    get :model, manufacturer: "Mitsubishi", model: "Parejo"
+    
+    assert_response :success
+    assert_template :partial => false
+  end
 end

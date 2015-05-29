32fc699bf66911794b31c199fa6a2333cb4c090e
Mwaki Magotswi <magotswi@gmail.com>
2015-05-29 16:34:40 -0400

    Finishing up manufacturer and model summary
    Cleaned up routes
    Did some initial drying of code
    Made modal clicks fetch info via AJAX

--- app/controllers/wikipedia_controller.rb
+++ app/controllers/wikipedia_controller.rb
@@ -1,2 +1,10 @@
 class WikipediaController < ApplicationController
+  include WikipediaHelper
+  def manufacturer
+    render plain: get_manufacturer_summary
+  end
+  
+  def model
+    render plain: get_model_summary
+  end
 end

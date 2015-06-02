a8028d0d42bd7980fc434b093304d9d922be746a
Mwaki Magotswi <magotswi@gmail.com>
2015-06-02 03:40:43 -0400

    removing unsued code

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,25 +1,2 @@
 module PagesHelper
-
-  def display_car_info(car_information)
-
-    car, year, suggested_price = car_information.first, car_information[1], car_information.last
-    manufacturer, *model = car.split
-  end
-
-  def car_details(car_information)
-    car_information.inject(content(:ul, nil, class: "manufacturer")) do |collection, car|
-      collection << content_tag(:li, car)
-    end
-  end
-
-  def car_information(cars)
-    content_tag :ul do
-      cars.collect {|car_detail| content_tag(:li, car_detail)}
-    end
-  end
-
-  def car_id(car_value)
-    car_value > 10000 ? "gooder" : "good"
-  end
-
 end

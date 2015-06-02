8e7cc4e4ab38cd5b109f43ffdc313e1bb663bf48
Mwaki Magotswi <magotswi@gmail.com>
2015-06-02 03:49:58 -0400

    more pending tests

--- test/helpers/wikipedia_helper_test.rb
+++ test/helpers/wikipedia_helper_test.rb
@@ -2,15 +2,20 @@ require 'test_helper'
 
 class WikipediaHelperTest < ActionView::TestCase
   
+  test "get wikipedia summary" do
+    skip "Awaiting refactor"
+    urls = ""
+    assert_equal get_wikipedia_summary(urls), ""
+  end
+  
   test "get manufacturer summary" do
     skip "Awaiting refactor"
     assert_equal get_manufacturer_summary, ""
   end
   
-  test "get wikipedia summary" do
+  test "get model summary" do
     skip "Awaiting refactor"
-    urls = ""
-    assert_equal get_wikipedia_summary(urls), ""
+    assert_equal get_model_summary, ""
   end
   
   test "fetch article" do
@@ -19,7 +24,7 @@ class WikipediaHelperTest < ActionView::TestCase
     assert_equal fetch_article(urls), ""
   end
   
-  test "fetch_article_data(urls)" do
+  test "fetch article data" do
     skip "Awaiting refactor"
     urls = ""
     assert_equal fetch_article_data(urls), ""
@@ -31,14 +36,28 @@ class WikipediaHelperTest < ActionView::TestCase
     assert_equal fetch_article_summary(article_data), ""
   end
   
-  test "get model summary" do
-    skip "Awaiting refactor"
-    assert_equal get_model_summary, ""
-  end
-  
   test "stanitze model names" do
     skip "Awaiting refactor"
     assert_equal stanitze_model_names, ""
   end
   
+  test "wikipediarize model" do
+    skip "Awaiting refactor"
+    assert_equal wikipediarize_model, ""
+  end
+  
+  test "clean up model" do
+    skip "Awaiting refactor"
+    assert_equal clean_up_model, ""
+  end
+  
+  test "build data url" do
+    skip "Awaiting refactor"
+    assert_equal build_data_url, ""
+  end
+  
+  test "build resource url" do
+    skip "Awaiting refactor"
+    assert_equal build_resource_url, ""
+  end
 end

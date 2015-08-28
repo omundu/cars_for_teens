e9126c081466e1ceaa156bd2bfcd021779603537
Mwaki Magotswi <magotswi@gmail.com>
2015-08-28 12:09:34 -0400

    adding ratings to the categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,7 +19,10 @@ class CategoriesController < ApplicationController
   end
   
   def year
+    manufacturer, model = params[:model].split
+    
     @title = "#{params[:model]} #{params[:year]}"
+    @safety_rating = SafetyRatings.new({manufacturer: manufacturer, model: model, year: params[:year]})
   end
   
 end
